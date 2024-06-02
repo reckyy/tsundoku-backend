36c106b13d4cd777eed30a6931a19fdd216583be
reckyy <srreki98@gmail.com>
2024-06-02 12:02:30 +0900

    💯 test: Api::UsersController#createのテスト追加

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -9,8 +9,6 @@ module Api
       else
         render json: {error: 'ログインに失敗しました' }, status: unprocessable_entity
       end
-    rescue ActiveRecord::RecordInvalid => e
-      render json: { error: e.message }, status: :unprocessable_entity
     rescue StandardError => e
       render json: { error: e.message }, status: :internal_server_error
     end
--- spec/requests/api/users_spec.rb
+++ spec/requests/api/users_spec.rb
@@ -2,8 +2,22 @@
 
 require 'rails_helper'
 
-RSpec.describe "Api::Users", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Api::Users', type: :request do
+  describe 'Api::UsersController' do
+    context 'params is valid' do
+      it 'return a successful response' do
+        user_params = { user: { name: 'hoge', email: 'hogehoge@example.com', avatar_url: 'https://hogehoge' } }
+        post api_auth_callback_google_path, params: user_params
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'params is not valid' do
+      it 'return a bad response' do
+        user_params = { user: { name: 'hoge', avatar_url: 'https://hogehoge' } }
+        post api_auth_callback_google_path, params: user_params
+        expect(response).to have_http_status(:internal_server_error)
+      end
+    end
   end
 end

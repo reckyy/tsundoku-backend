66447092102542a174bc17772d03763f056fc60f
reckyy <srreki98@gmail.com>
2024-06-30 13:22:46 +0900

    💯 test: rubocop

--- db/migrate/20240630024222_create_headings.rb
+++ db/migrate/20240630024222_create_headings.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateHeadings < ActiveRecord::Migration[7.1]
   def change
     create_table :headings do |t|
--- spec/factories/headings.rb
+++ spec/factories/headings.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :heading do
     number { 1 }
-    title { "MyString" }
+    title { 'MyString' }
     user_book { nil }
   end
 end

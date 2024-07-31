157e6bc5ab050ee299619fece8439799e9d4d732
reckyy <srreki98@gmail.com>
2024-07-31 19:10:53 +0900

    🍀 refactor: Fat controllerの解消

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -9,35 +9,15 @@ module Api
     end
 
     def create
-      book = Book.find_or_create_by!(book_params)
-      if book && create_user_book(book, current_user)
+      title = params[:title]
+      author = params[:author]
+      cover_image_url = params[:cover_image_url]
+      book = Book.find_or_initialize_by(title:, author:, cover_image_url:)
+      if book.save_with_user_book(current_user, params[:heading_number]&.to_i)
         head :ok
       else
         render json: { error: '本の登録に失敗しました' }, status: :unprocessable_entity
       end
-    rescue StandardError => e
-      render json: { error: e.message }, status: :internal_server_error
-    end
-
-    private
-
-    def book_params
-      params.permit(:title, :author, :cover_image_url)
-    end
-
-    def create_user_book(book, user)
-      user_book = UserBook.find_or_create_by!(user:, book:)
-      heading_number = params[:heading_number]&.to_i
-      return false if heading_number.nil?
-
-      insert_chapter(user_book, heading_number)
-    end
-
-    def insert_chapter(user_book, heading_number)
-      (1..heading_number).map do |n|
-        user_book.headings.create!(number: n, title: "Chapter #{n}", memo_attributes: {})
-      end
-      true
     end
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -9,4 +9,28 @@ class Book < ApplicationRecord
 
   has_many :user_books, dependent: :destroy
   has_many :users, through: :user_books
+
+  def save_with_user_book(user, heading_number)
+    Book.transaction do
+      succeeded = save
+      succeeded = save_user_book(user, heading_number) if succeeded
+      raise ActiveRecord::Rollback unless succeeded
+
+      true
+    end
+  end
+
+  def save_user_book(user, heading_number)
+    user_book = UserBook.find_or_create_by!(user:, book: self)
+    return false if heading_number.zero? # もしheading_numberがnilなら0になるため
+
+    insert_chapter(user_book, heading_number)
+  end
+
+  def insert_chapter(user_book, heading_number)
+    (1..heading_number).map do |n|
+      user_book.headings.create!(number: n, title: '', memo_attributes: {})
+    end
+    true
+  end
 end

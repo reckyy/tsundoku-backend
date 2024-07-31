f13381b3daec01483c0471d70ab1bc6595fdfe17
reckyy <srreki98@gmail.com>
2024-07-31 16:46:08 +0900

    🍀 refactor: Fat controllerを解消

--- app/controllers/api/memos_controller.rb
+++ app/controllers/api/memos_controller.rb
@@ -5,8 +5,8 @@ module Api
     before_action :authenticate, only: [:index]
 
     def index
-      user_book = UserBook.find_by(user: current_user, book_id: params[:book_id])
-      book_with_memos = build_book_with_memos(user_book)
+      user_book = UserBook.preload(headings: :memo).find_by(user: current_user, book_id: params[:book_id])
+      book_with_memos = { book: user_book.book, headings: user_book.headings.as_json(include: :memo) }
       render json: book_with_memos
     end
 
@@ -26,26 +26,6 @@ module Api
 
     def memo_params
       params.require(:memo).permit(:id, :body)
-    end
-
-    def build_book_with_memos(user_book)
-      {
-        book: {
-          title: user_book.book.title,
-          cover_image_url: user_book.book.cover_image_url
-        },
-        headings: user_book.headings.map do |heading|
-          {
-            id: heading.id,
-            number: heading.number,
-            title: heading.title,
-            memo: {
-              id: heading.memo.id,
-              body: heading.memo.body
-            }
-          }
-        end
-      }
     end
   end
 end

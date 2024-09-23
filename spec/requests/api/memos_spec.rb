abc182a5887905d287cdab1eeeff63f34d19e879
reckyy <srreki98@gmail.com>
2024-09-23 17:31:57 +0900

    🐛 fix: 不要なルーティングのネストを削除
    また、MemosController#updateのテストも不足していたので追加

--- spec/requests/api/memos_spec.rb
+++ spec/requests/api/memos_spec.rb
@@ -5,7 +5,8 @@ require 'rails_helper'
 RSpec.describe 'API::Memos', type: :request do
   before do
     @user = FactoryBot.create(:user)
-    FactoryBot.create(:reading_log)
+    reading_log = FactoryBot.create(:reading_log)
+    @memo = reading_log.memo
   end
 
   describe 'API::MemosController#index' do
@@ -14,6 +15,18 @@ RSpec.describe 'API::Memos', type: :request do
         params = { user_id: @user.id }
         get(api_reading_logs_path, params:)
         expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+
+  describe 'API::MemosController#update' do
+    context 'params is valid' do
+      it 'return a successful response and update the data successfully' do
+        params = { memo: { id: @memo.id, body: '更新後のメモ' }, user_id: @user.id }
+        patch(api_memo_path(@memo.id), params:)
+        expect(response).to have_http_status(:ok)
+        @memo.reload
+        expect(@memo.body).to eq('更新後のメモ')
       end
     end
   end

3a47f7322a8af36411599e9fec1ea4f49f1273b6
reckyy <srreki98@gmail.com>
2025-03-23 12:40:52 +0900

    🐛 fix: Bookモデルのバリデーションをtitleのみ必須にするように変更

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 class Book < ApplicationRecord
-  with_options presence: true do
-    validates :title
-    validates :author
-    validates :cover_image_url
-  end
+  validates :title, presence: true
 
   has_many :user_books, dependent: :destroy
   has_many :users, through: :user_books

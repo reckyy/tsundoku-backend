25b2386bdb6ebae75004548e2cb2cf9e00af0813
reckyy <srreki98@gmail.com>
2025-03-25 22:18:54 +0900

    💯 test: UserBookWithMemosResourceのテストを作成

---
+++ spec/resources/user_book_with_memos_resource_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe UserBookWithMemosResource, type: :resource do
+  before do
+    user = FactoryBot.create(:user)
+    @book = FactoryBot.create(:book)
+    @user_book = UserBook.create(user:, book: @book)
+    @heading = FactoryBot.create(:heading, user_book: @user_book)
+    @memo = FactoryBot.create(:memo, heading: @heading)
+  end
+
+  it 'returns the attributes of the book' do
+    user_book_with_memos_json = UserBookWithMemosResource.new(@user_book).serializable_hash.to_json
+    expected_book_json = {
+      id: @user_book.id,
+      status: @user_book.status,
+      book: {
+        id: @book.id,
+        title: @book.title,
+        author: @book.author,
+        coverImageUrl: @book.cover_image_url
+      },
+      headings: [{
+        id: @heading.id,
+        number: @heading.number,
+        title: @heading.title,
+        memo: {
+          id: @memo.id,
+          body: @memo.body
+        }
+      }]
+    }.to_json
+    expect(user_book_with_memos_json).to eq(expected_book_json)
+  end
+end

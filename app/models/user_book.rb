4d617c697c228eab01a67109e2cdc782e8c30ced
recky <srreki98@gmail.com>
2026-06-17 22:26:29 +0900

    refactor: user_book.rbの未使用スコープstatus_*_orderedを削除

--- app/models/user_book.rb
+++ app/models/user_book.rb
@@ -11,10 +11,6 @@ class UserBook < ApplicationRecord
 
   acts_as_list scope: :user
 
-  scope :status_unread_ordered, -> { status_unread.order(:position) }
-  scope :status_reading_ordered, -> { status_reading.order(:position) }
-  scope :status_finished_ordered, -> { status_finished.order(:position) }
-
   def swap_positions_with(item)
     transaction do
       item_position = item.position
